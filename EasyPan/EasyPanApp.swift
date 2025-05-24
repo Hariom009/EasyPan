331e92886bbc74ccea84f3e9d1116163cb0138b1
Hariom009 <hariomconnects03@gmail.com>
2025-05-25 03:46:40 +0530

    Initial Commit

---
+++ EasyPan/EasyPanApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyPanApp.swift
+//  EasyPan
+//
+//  Created by Hari's Mac on 25.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct EasyPanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
